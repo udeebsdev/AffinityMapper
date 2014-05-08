9dc4d83daf29d4d6508cb83665383baa848ea3ac
Prapti Shrestha <pshrestha@vaddio.com>
2014-05-07 19:24:24 -0500

    adding initial interests activity

---
+++ affinityMapper/src/main/java/com/affinitymapper/affinitymapper/activities/Interests.java
@@ -0,0 +1,51 @@
+package com.affinitymapper.affinitymapper.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.affinitymapper.affinitymapper.R;
+import android.widget.Toast;
+
+public class Interests extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interests);
+    }
+
+    public void sportsClicked(View view)
+    {
+        Toast.makeText(getApplicationContext(), "Sports button is clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void literatureClick(View view)
+    {
+        Toast.makeText(getApplicationContext(), "Literature button is clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void historyClick(View view)
+    {
+        Toast.makeText(getApplicationContext(), "History button is clicked", Toast.LENGTH_LONG).show();
+    }
+
+    public void PoliticsClick(View view)
+    {
+        Toast.makeText(getApplicationContext(), "Politics button is clicked", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
